754cce67b3cd1d69e9de0d5e5a66343a7e9d24b6
Floodlite <agiphy98@gmail.com>
2026-09-06 23:06:28 -0700

    feat(Water): Bolstered Buoyancy
    Added more stuff to buoyancy
    
    TODO: Floaters will move independently and detach from their parent object, maybe due to rigidbody settings

--- Fluids/Buoyancy.cs
+++ Fluids/Buoyancy.cs
@@ -11,19 +11,34 @@ public class Buoyancy : MonoBehaviour
     [SerializeField] private float depthBeforeSubmerged = -0.1f;
     [SerializeField] private float displacementAmount = 3f;
     [SerializeField] private int objectCount = 0;
+    private bool floatersOnly = true;
+
+    //Waves
+    [SerializeField] private bool waves = false;
+    [SerializeField] private float amplitude = 1f;
+    [SerializeField] private float length = 2f;
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private float offset = 0f;
 
 
     private void FixedUpdate()
     {
+        offset += Time.deltaTime * speed;
+
         if(floatingObjects.Count <= 0) { return; }
         foreach(GameObject objectum in floatingObjects.Keys.ToList())
         {
             Transform tform = objectum.transform;
-            float waveHeight = WaterWave.instance.GetWaveHeight(tform.position.x);
+            float waveHeight = GetWaveHeight(tform.position.x);
             //if(objectum.transform.position.y < this.gameObject.transform.position.y)
-            if(objectum.transform.position.y < waveHeight)
+            if(waves && objectum.transform.position.y < waveHeight)
             {
                 //Debug.Log("Applying forces");
+                float displacementMultiplier = Mathf.Clamp01((waveHeight - tform.position.y) / depthBeforeSubmerged) * displacementAmount;
+                floatingObjects[objectum].AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), ForceMode.Acceleration);
+            }
+            else if(!waves && objectum.transform.position.y < this.transform.position.y)
+            {
                 float displacementMultiplier = Mathf.Clamp01(-tform.position.y / depthBeforeSubmerged) * displacementAmount;
                 floatingObjects[objectum].AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), ForceMode.Acceleration);
             }
@@ -34,8 +49,17 @@ public class Buoyancy : MonoBehaviour
         }
     }
 
+
+    public float GetWaveHeight(float x)
+    {
+        return amplitude * Mathf.Sin(x / length + offset);
+    }
+
+
+
     private void OnTriggerEnter(Collider other)
     {
+        if(floatersOnly && !other.CompareTag("Floater")) { return; }
         GameObject obj = other.gameObject;
         Rigidbody rb = obj.GetComponent<Rigidbody>();
         if(rb == null) { obj.GetComponentInChildren<Rigidbody>(); }
